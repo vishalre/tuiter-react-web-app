cf979cfc340710a6505238a62890f796d6a49922
Vishal Reddy Gurrala <gurrala.v@northeastern.edu>
2022-11-19 11:20:01 -0600

    like onclickhandler

--- src/tuiter/Tuits/TuitStats.js
+++ src/tuiter/Tuits/TuitStats.js
@@ -1,10 +1,13 @@
 import {updateTuitThunk}
     from "../../services/tuits-thunks";
 import {useDispatch} from "react-redux";
+import {useState} from "react";
 
 const TuitStats = ({stats}) => {
+    const [isActive, setIsActive] = useState(false);
     const dispatch = useDispatch();
     const likeClickHandler = () => {
+        setIsActive(true);
         dispatch(updateTuitThunk({
                       ...stats,
                       likes: stats.likes + 1
@@ -29,7 +32,7 @@ const TuitStats = ({stats}) => {
             <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">
                 <i onClick={likeClickHandler}
                    className='fa fa-heart'
-                   style={{"color": `${stats.liked === true ? "red" : "gray"}`}}></i>
+                   style={{"color": `${(stats.liked === true || isActive === true) ? "red" : "gray"}`}}></i>
                 <span className='ms-1 wd-gray-color'>{stats.likes}</span>
             </div>
             <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">
